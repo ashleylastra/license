e7cbb0806122f7716fc23565c4d05f59d71a7bc1
Michael Vanacore <mvanacore@seneca-solutions>
2019-10-23 13:59:11 -0400

    Wired up the env vars to ocp.

--- inventory-master/includes/config.php
+++ inventory-master/includes/config.php
@@ -11,9 +11,17 @@
 |
 |
 */
-  define( 'DB_HOST', 'localhost' );          // Set database host
-  define( 'DB_USER', 'root' );             // Set database user
-  define( 'DB_PASS', 'Wally13!' );             // Set database password
-  define( 'DB_NAME', 'inventorymanager' );        // Set database name
+
+  #define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_HOST', getenv('DB_HOST', true) ?: getenv('DB_HOST') );          // Set database host
+  
+  #define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_USER', getenv('DB_USER', true) ?: getenv('DB_USER') );             // Set database user
+  
+  #define( 'DB_PASS', 'Wally13!' );             // Set database password
+  define( 'DB_PASS', getenv('DB_PASS', true) ?: getenv('DB_PASS') );             // Set database password
+  
+  #define( 'DB_NAME', 'inventorymanager' );        // Set database name
+  define( 'DB_NAME', getenv('DB_NAME', true) ?: getenv('DB_NAME') );        // Set database name
 
 ?>
